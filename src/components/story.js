926fafa192e78d1ae3200eecc65fbf4f49b4b550
Vladyslav <ganushkevychv@gmail.com>
2020-07-09 00:30:03 +0200

    bg-color was changed, new cards added to story.js

--- src/components/story.js
+++ src/components/story.js
@@ -1,19 +1,32 @@
 import React from "react"
 import { Card, CardTitle, CardBody, CardText, CardImg} from "reactstrap"
 import back from "../images/back.png"
-const Story = (props) => {
+const Story = () => {
     const card = {
         boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
         alignItems:"center",
         marginLeft:"50px",
         marginRight:"50px",
+        marginBottom:"30px",
     }
     return(
+
 <div style={{textAlign:"center"}}>
-    
             <Card style={card}>
             <CardImg src={back}/>
             <CardBody >
+                <CardTitle className="text-center text-uppercase">Саша и Таня</CardTitle>
+              <CardText className="text-center text-uppercase">
+              <p>
+                gksjdjSDJVJ OJOW JWOJ WJ EPWJ EPJRWPE POW JPE JPOWEJ POWJEPJ WPOJE POJWPJ JWPJ POJWPO JWPO
+              </p>
+              </CardText>
+            </CardBody>
+            </Card>
+            <Card style={card}>
+            <CardImg src={back}/>
+            <CardBody >
+                <CardTitle className="text-center text-uppercase">Саша и Таня</CardTitle>
               <CardText className="text-center text-uppercase">
               <p>
                 gksjdjSDJVJ OJOW JWOJ WJ EPWJ EPJRWPE POW JPE JPOWEJ POWJEPJ WPOJE POJWPJ JWPJ POJWPO JWPO
